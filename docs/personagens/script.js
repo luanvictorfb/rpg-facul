93e713ecb91605e1046bec1e5b557e3b2dab8ae0
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-05-21 20:35:48 -0300

    Add files via upload

--- docs/personagens/script.js
+++ docs/personagens/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const proficiencia = document.querySelector('input[name="bonus_proficiencia"]');
+  const bonus = document.querySelector('input[name="bonusArmadura"]');
+  const armadura = document.querySelector('input[name="armadura"]');
 
   const atributos = {
     forca: document.querySelector('input[name="forca"]'),
@@ -34,10 +36,14 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function calcArma(){
+    armadura.value = bonus+10+calculaModificador(destreza.value);
+  }
+
   for (const i in atributos) {
     atributos[i].addEventListener('input', atualizaTestes);
   }
-
+  calcArma(destreza.value)
   atualizaTestes();
 });
 
@@ -47,51 +53,6 @@ const classContainer = document.querySelector('.class');
 let xp = document.querySelector('input[name="xp"]')
 let classeCount = 1;
 const classeContainer = document.querySelector('.class');
-
-document.querySelector('.addSpell').addEventListener('click', (e) => {
-  e.preventDefault();
-  spellCount++
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('input-spells');
-  if (spellCount === 2) {
-    spellContainer.style.gridTemplateColumns = '1fr 1fr';
-  } else if (spellCount === 3) {
-    spellContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
-  }
-
-  newDiv.innerHTML = `
-      <label>Nome da Magia: 
-        <input type="text" name="spell_name_${spellCount}" />
-      </label>
-      <label>Usos Restantes: 
-        <input type="number" name="spell_uses_${spellCount}" min="0" />
-      </label>
-      <label>Descrição:</label>
-      <textarea name="spell_desc_${spellCount}" rows="3"></textarea>
-    `;
-  spellContainer.appendChild(newDiv);
-
-});
-
-document.querySelector('.removeSpell').addEventListener('click', (e) => {
-  e.preventDefault();
-  const spells = spellContainer.querySelectorAll('.input-spells');
-
-  if (spells.length > 1) {
-    spells[spells.length - 1].remove();
-    spellCount--;
-
-    if (spellCount === 2) {
-      spellContainer.style.gridTemplateColumns = '1fr 1fr';
-    } else if (spellCount === 3) {
-      spellContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
-    } else {
-      spellContainer.style.gridTemplateColumns = '1fr';
-    }
-  } else {
-    alert("Você precisa ter pelo menos uma magia.");
-  }
-});
 
 document.querySelector('.addClasse').addEventListener('click', (e) => {
   e.preventDefault();
@@ -120,15 +81,63 @@ document.querySelector('.removeClasse').addEventListener('click', (e) => {
     allClasseDivs[allClasseDivs.length - 1].remove();
     classeCount--;
 
-    if (classeCount === 2) {
-      classContainer.style.gridTemplateColumns = '1fr 1fr';
-    } else if (classeCount === 3) {
-      classContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
-    } else {
+    if (classeCount === 1) {
       classContainer.style.gridTemplateColumns = '1fr';
+    } else if (classeCount === 2) {
+      classContainer.style.gridTemplateColumns = '1fr 1fr';
+    } else {
+      classContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
     }
   } else {
     alert("Você precisa ter pelo menos uma classe.");
+  }
+}); 
+
+document.querySelector('.addSpell').addEventListener('click', (e) => {
+  e.preventDefault();
+  spellCount++
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('input-spells');
+  if (spellCount === 2) {
+    spellContainer.style.gridTemplateColumns = '1fr 1fr';
+  } else if (spellCount === 3) {
+    spellContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
+  }
+
+  newDiv.innerHTML = `
+      <label>Nome da Magia: 
+        <input type="text" name="spell_name_${spellCount}" />
+      </label>
+      <label>Usos Restantes: 
+        <input type="number" name="spell_uses_${spellCount}" min="0" />
+      </label>
+      <label>Descrição:</label>
+      <textarea name="spell_desc_${spellCount}" rows="3"></textarea>
+    `;
+  spellContainer.appendChild(newDiv);
+
+});
+
+
+
+
+document.querySelector('.removeSpell').addEventListener('click', (e) => {
+  e.preventDefault();
+  const spells = spellContainer.querySelectorAll('.input-spells');
+
+  if (spells.length > 1) {
+    spells[spells.length - 1].remove();
+    spellCount--;
+
+    if (spellCount === 1) {
+      spellContainer.style.gridTemplateColumns = '1fr';
+    } else if (spellCount === 2) {
+      spellContainer.style.gridTemplateColumns = '1fr 1fr';
+    } else {
+      spellContainer.style.gridTemplateColumns = '1fr 1fr 1fr';
+    }
+  } else {
+    alert("Você precisa ter pelo menos uma magia.");
   }
 });
 
@@ -162,64 +171,64 @@ function calcularNivelTotal() {
 
 function modifAtri() {
   if (infoGerais.xp.value >= 0 && infoGerais.xp.value <= 299) {
-    bonusState.proficiencia.value = 2;
+    proficiencia.value = 2;
     infoGerais.nivel.value = 1;
   } else if (infoGerais.xp.value >= 300 && infoGerais.xp.value <= 899) {
-    bonusState.proficiencia.value = 2;
+    proficiencia.value = 2;
     infoGerais.nivel.value = 2;
   } else if (infoGerais.xp.value >= 900 && infoGerais.xp.value <= 2699) {
-    bonusState.proficiencia.value = 2;
+    proficiencia.value = 2;
     infoGerais.nivel.value = 3;
   } else if (infoGerais.xp.value >= 2700 && infoGerais.xp.value <= 6499) {
-    bonusState.proficiencia.value = 3;
+    proficiencia.value = 3;
     infoGerais.nivel.value = 4;
   } else if (infoGerais.xp.value >= 6500 && infoGerais.xp.value <= 13999) {
-    bonusState.proficiencia.value = 3;
+    proficiencia.value = 3;
     infoGerais.nivel.value = 5;
   } else if (infoGerais.xp.value >= 14000 && infoGerais.xp.value <= 22999) {
-    bonusState.proficiencia.value = 3;
+    proficiencia.value = 3;
     infoGerais.nivel.value = 6;
   } else if (infoGerais.xp.value >= 23000 && infoGerais.xp.value <= 33999) {
-    bonusState.proficiencia.value = 3;
+    proficiencia.value = 3;
     infoGerais.nivel.value = 7;
   } else if (infoGerais.xp.value >= 34000 && infoGerais.xp.value <= 47999) {
-    bonusState.proficiencia.value = 3;
+    proficiencia.value = 3;
     infoGerais.nivel.value = 8;
   } else if (infoGerais.xp.value >= 48000 && infoGerais.xp.value <= 63999) {
-    bonusState.proficiencia.value = 4;
+    proficiencia.value = 4;
     infoGerais.nivel.value = 9;
   } else if (infoGerais.xp.value >= 64000 && infoGerais.xp.value <= 84999) {
-    bonusState.proficiencia.value = 4;
+    proficiencia.value = 4;
     infoGerais.nivel.value = 10;
   } else if (infoGerais.xp.value >= 85000 && infoGerais.xp.value <= 99999) {
-    bonusState.proficiencia.value = 4;
+    proficiencia.value = 4;
     infoGerais.nivel.value = 11;
   } else if (infoGerais.xp.value >= 100000 && infoGerais.xp.value <= 119999) {
-    bonusState.proficiencia.value = 4;
+    proficiencia.value = 4;
     infoGerais.nivel.value = 12;
   } else if (infoGerais.xp.value >= 120000 && infoGerais.xp.value <= 139999) {
-    bonusState.proficiencia.value = 5;
+    proficiencia.value = 5;
     infoGerais.nivel.value = 13;
   } else if (infoGerais.xp.value >= 140000 && infoGerais.xp.value <= 164999) {
-    bonusState.proficiencia.value = 5;
+    proficiencia.value = 5;
     infoGerais.nivel.value = 14;
   } else if (infoGerais.xp.value >= 165000 && infoGerais.xp.value <= 194999) {
-    bonusState.proficiencia.value = 5;
+    proficiencia.value = 5;
     infoGerais.nivel.value = 15;
   } else if (infoGerais.xp.value >= 195000 && infoGerais.xp.value <= 224999) {
-    bonusState.proficiencia.value = 5;
+    proficiencia.value = 5;
     infoGerais.nivel.value = 16;
   } else if (infoGerais.xp.value >= 225000 && infoGerais.xp.value <= 264999) {
-    bonusState.proficiencia.value = 6;
+    proficiencia.value = 6;
     infoGerais.nivel.value = 17;
   } else if (infoGerais.xp.value >= 265000 && infoGerais.xp.value <= 304999) {
-    bonusState.proficiencia.value = 6;
+    proficiencia.value = 6;
     infoGerais.nivel.value = 18;
   } else if (infoGerais.xp.value >= 305000 && infoGerais.xp.value <= 354999) {
-    bonusState.proficiencia.value = 6;
+    proficiencia.value = 6;
     infoGerais.nivel.value = 19;
   } else {
-    bonusState.proficiencia.value = 6;
+    proficiencia.value = 6;
     infoGerais.nivel.value = 20;
   }
 }
